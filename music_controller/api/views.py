ba4f5c76e7c4a2b06f5bc363aa2221e8307ec64a
Hoa Bui <107874148+hoab02@users.noreply.github.com>
2024-08-21 11:15:42 +0700

    Initial commit

---
+++ music_controller/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from .serializers import RoomSerializer
+from .models import Room
+from rest_framework import generics
+
+# Create your views here.
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
